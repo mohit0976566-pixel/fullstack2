e8bcfc5cabbd533f55b9111451b7751cfd9ce1ee
VSCode User <vscode@example.com>
2026-09-08 11:25:29 +0530

    Prevent duplicate render count in development

--- Exp1.4/Exp1.4/1.44/interactive-calendar/src/main.jsx
+++ Exp1.4/Exp1.4/1.44/interactive-calendar/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
@@ -7,9 +6,7 @@ import './index.css';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
